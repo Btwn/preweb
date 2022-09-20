2b3ad0e0c2ce937a520acc758e02209979f7a3d1
Calos Alberto Diaz Jimenez <carlos4diazj@hotmail.com>
2022-09-20 00:48:39 -0500

    simetria

---
+++ kube/asimetrico.cs
@@ -0,0 +1,31 @@
+// Asimetrico
+
+class TareaRepository
+{
+    public void Create(string titulo, bool completado){
+      // ..
+    }
+
+    public Tarea Find(int id){
+      // ..
+    }
+
+    public void Update(Tarea tarea){
+      // ..
+    }
+
+    public void Destroy(int id){
+      // ..
+    }
+}
+
+
+class Job
+{
+    public precess()
+    {
+        this.input();
+        this.count++;
+        this.output();
+    }
+}
---
+++ kube/simetrico.cs
@@ -0,0 +1,31 @@
+// Simetrico
+
+class TareaRepository
+{
+    public void Create(Tarea tarea){
+      // ..
+    }
+
+    public Tarea Read(int id){
+      // ..
+    }
+
+    public void Update(Tarea tarea){
+      // ..
+    }
+
+    public void Delete(int id){
+      // ..
+    }
+}
+
+
+class Job
+{
+    public precess()
+    {
+        this.input();
+        this.incrementa();
+        this.output();
+    }
+}
